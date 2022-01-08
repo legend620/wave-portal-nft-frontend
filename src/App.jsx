18a77221c98fdd0b9f02e5e241ab393f8eaeb3e5
legend620 <50647994+legend620@users.noreply.github.com>
2022-01-08 15:36:49 +0000

    update bio

--- src/App.jsx
+++ src/App.jsx
@@ -147,7 +147,7 @@ export default function App() {
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+          Connect your Ethereum wallet and wave at me!
         </div>
 
         {!currentAccount ? (
